8608dfcfe0770498c9807c271d919ae88b12fa35
Cordell <cordellbonnieux@gmail.com>
2022-07-24 22:04:55 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Quiz App</title>
+    <meta name="description" content="Quiz App">
+    <meta name="author" content="Cordell Bonnieux">
+
+    <link rel="icon" href="/favicon.ico">
+
+    <link rel="stylesheet" href="css/styles.css?v=1.0">
+    <script src="script.js" defer></script>
+</head>
+<body>
+
+</body>
+</html>
