850fa5af6fe5b73810d33e8ca34d11dce56f4ddb
Cordell <cordellbonnieux@gmail.com>
2022-07-28 15:32:38 -0700

    Added login and register form html

--- login.php
+++ login.php
@@ -14,6 +14,45 @@
     <script src="js/script.js" defer></script>
 </head>
 <body>
-    <h1>HEY</h1>
+    <h1>Quiz App</h1>
+    <div class="wrapper">
+        <button id="toggleLogin">create account</button>
+        <div id="login">
+            <h2>Login</h1>
+            <form action="index.php" method="post">
+                <label for="userLogin">
+                    username or email:
+                    <input type="text" id="userLogin" name="userLogin"></input>
+                </label>
+                <label for="passwordLogin">
+                    password:
+                    <input type="password" id="passwordLogin" name="passwordLogin"></input>
+                </label>
+                <button type="submit" name="submitLogin">login</button>
+            </form>
+        </div>
+        <div id="register">
+            <h2>Create an account</h2>
+            <form action="index.php" method="post">
+                <label for="userRegister">
+                    username:
+                    <input type="text" id="userRegister" name="userRegister"></input>
+                </label>
+                <label for="emailRegister">
+                    email:
+                    <input type="text" id="emailRegister" name="emailRegister"></input>
+                </label>
+                <label for="passwordRegister">
+                    password:
+                    <input type="password" id="passwordRegister" name="passwordRegister"></input>
+                </label>
+                <label for="passwordRegister2">
+                    password, again:
+                    <input type="password" id="passwordRegister2" name="passwordRegister2"></input>
+                </label>
+                <button type="submit" name="submitLogin">login</button>
+            </form>
+        </div>
+    </div>
 </body>
 </html>
