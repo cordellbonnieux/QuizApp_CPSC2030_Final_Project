22093bcdeb836975638f6f1c2a2d5078d77b57e8
Cordell <cordellbonnieux@gmail.com>
2022-08-02 10:33:04 -0700

    Start ignoring database/config.php

--- database/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Define the constants for host, dbname, username and password
-define('DBHOST', 'localhost');
-define('DBNAME', 'project2030');
-define('DBUSER', 'root');
-define('DBPASS', '');
-define('UNSPLASH_SECRET', 'NmsbcQxQA6yzPwzTBZfF8jF3Exfakj22EHVfJwvd1lc');
-define('UNSPLASH_ACCESS', 'YKrSKDkWtiHZi2XLIPzQPFTExaO4dcwI86rGbCdijIM');
- ?>
